d65a2eca21ed98c1a10439641f1a23ce800945bf
James A. Brannan <jamesabrannan@hushmail.com>
2023-10-05 14:45:19 -0400

    more additions

---
+++ Examples/HtmlToPdfByteArrayExample.cs
@@ -0,0 +1,17 @@
+﻿
+using ceTe.DynamicPDF.Conversion;
+using System;
+using System.IO;
+
+namespace DynamicPDFCoreSuite.Examples
+{
+
+    class HtmlToPdfByteArrayExample
+    {
+        public static void Run()
+        {
+            byte[] pdfByteArray = Converter.Convert("https://www.google.com");
+            File.WriteAllBytes(Util.GetPath("Output/html-byte-array-output.pdf"), pdfByteArray);
+        }
+    }
+}

d2ff8872e948539579123515c4e6e15d22907090
James A. Brannan <jamesabrannan@hushmail.com>
2024-08-07 16:59:51 -0400

    Added stamp template example

---
+++ Utility/TextGenerator.cs
@@ -0,0 +1,14 @@
+﻿
+using DynamicPDFCoreSuite.Examples;
+using System.IO;
+
+namespace DynamicPDFCoreSuite.Utility
+{
+    public class TextGenerator
+    {
+        public static string Generate()
+        {
+            return File.ReadAllText(Util.GetPath("Resources/HTML/pglatin.html"));
+        }
+    }
+}

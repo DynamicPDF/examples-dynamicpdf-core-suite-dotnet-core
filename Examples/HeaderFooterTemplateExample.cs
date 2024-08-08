0ab93ab0b5235390a58de04ec0d595616a75062d
James A. Brannan <jamesabrannan@hushmail.com>
2024-08-08 10:20:30 -0400

    Added header footer template example

---
+++ Examples/HeaderFooterTemplateExample.cs
@@ -0,0 +1,27 @@
+﻿
+using ceTe.DynamicPDF;
+using ceTe.DynamicPDF.PageElements;
+using DynamicPDFCoreSuite.Utility;
+
+namespace DynamicPDFCoreSuite.Examples
+{
+    class HeaderFooterTemplateExample
+    {
+        public static void Run()
+        {
+            Document myDoc = new();
+            myDoc.Pages.Add(new Page());
+
+            HeaderFooterTemplate header = new HeaderFooterTemplate("Header text", "Footer text");
+            HeaderFooterText leftText = new HeaderFooterText("Example Header");
+            leftText.Font = Font.Helvetica;
+            leftText.FontSize = 12;
+            header.HeaderLeft = leftText;
+            myDoc.Template = header;
+
+            header.FooterLeft = header.FooterCenter;
+
+            myDoc.Draw(Util.GetPath("Output/header-footer-template-out.pdf"));
+        }
+    }
+}

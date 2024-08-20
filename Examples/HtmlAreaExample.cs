55a17f0869564e9781facfd88f710bc716532b57
James A. Brannan <jamesabrannan@hushmail.com>
2024-08-20 13:39:18 -0400

    Update HtmlAreaExample.cs

--- Examples/HtmlAreaExample.cs
+++ Examples/HtmlAreaExample.cs
@@ -13,11 +13,16 @@ namespace DynamicPDFCoreSuite.Examples
         public static void Run()
         {
             Document document = new Document();
+            Page page = new Page(PageSize.Letter);
+            document.Pages.Add(page);
 
-            Uri filePath = new Uri(Util.GetPath("Resources/HTML/example.html"));
+            float hgt = page.Dimensions.Height - page.Dimensions.TopMargin * 2;
+            float wdth = page.Dimensions.Width - page.Dimensions.LeftMargin * 2;
 
-            HtmlArea htmlArea = new HtmlArea(filePath, 0, 0, 500, 600);
-            Page page = new Page();
+            Uri filePath = new Uri(Util.GetPath("Resources/HTML/simple.html"));
+
+            HtmlArea htmlArea = new HtmlArea(filePath, 0, 0, wdth, hgt);
+            
             page.Elements.Add(htmlArea);
 
             document.Pages.Add(page);

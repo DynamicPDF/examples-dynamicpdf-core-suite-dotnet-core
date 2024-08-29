0357e56778fcee8b47105b73c358a150b2fedc3c
James A. Brannan <jamesabrannan@hushmail.com>
2024-08-29 13:28:11 -0400

    Update TableContinuationExample.cs

--- Examples/TableContinuationExample.cs
+++ Examples/TableContinuationExample.cs
@@ -7,6 +7,12 @@ namespace DynamicPDFCoreSuite.Examples
     {
         public static void Run()
         {
+            TableContinuationExample.OverflowRows();
+            TableContinuationExample.OverflowColumns();
+        }
+
+        public static void OverflowRows()
+        {
             // Create a PDF Document 
             Document document = new Document();
 
@@ -41,6 +47,47 @@ namespace DynamicPDFCoreSuite.Examples
 
             // Save the PDF 
             document.Draw(Util.GetPath("Output/table-continuation-output.pdf"));
+        }
+
+        public static void OverflowColumns()
+        {
+            // Create a PDF Document 
+            Document document = new Document();
+
+            Table2 table = new Table2(0, 0, 500, 50);
+
+            // create the headers for the two rows
+            table.Columns.Add(150);
+            table.Columns.Add(150);
+            Row2 row1 = table.Rows.Add(20);
+            Row2 row2 = table.Rows.Add(20);
+            row1.Cells.Add("Column Header");
+            row2.Cells.Add("Row Header");
+            table.RepeatRowHeaderCount = 2;
+
+            // create columns
+            int cols = 50;
+            for (int i = 0; i <= cols; i++)
+            {
+                table.Columns.Add(100);
+                for (int j = 0; j <= cols; j++)
+                {
+                    row1.Cells.Add("Column #" + j);
+                    row2.Cells.Add("Item");
+                }
+            }
+
+            // This loop outputs the table to as many pages as is required
+            do
+            {
+                Page page = new Page();
+                document.Pages.Add(page);
+                page.Elements.Add(table);
+                table = table.GetOverflowColumns();
+            } while (table != null);
+
+            // Save the PDF 
+            document.Draw(Util.GetPath("Output/table-column-continuation-output.pdf"));
         }
     }
 }

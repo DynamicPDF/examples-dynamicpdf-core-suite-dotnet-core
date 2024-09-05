dade791e7c6ec9ecd13358de4024614cf9371870
James A. Brannan <jamesabrannan@hushmail.com>
2024-09-05 09:50:09 -0400

    charting additions

--- Examples/BarChartsExample.cs
+++ Examples/BarChartsExample.cs
@@ -13,6 +13,9 @@ namespace DynamicPDFCoreSuite.Examples
             SimpleBarChart();
             SimpleBarChartIndexedAxis();
             SimpleBarChartManualLayout();
+            SimpleDoubleBarChart();
+
+
         }
 
         public static void SimpleBarChart()
@@ -109,6 +112,53 @@ namespace DynamicPDFCoreSuite.Examples
 
             page.Elements.Add(chart);
             document.Draw(Util.GetPath("Output/barchart-simple-manual-layout-output.pdf"));
+        }
+
+        public static void SimpleDoubleBarChart()
+        {
+            Document document = new Document();
+            Page page = new Page();
+            document.Pages.Add(page);
+
+            float hght = document.Pages[0].Dimensions.Height - document.Pages[0].Dimensions.TopMargin * 2;
+            float wdth = document.Pages[0].Dimensions.Width - document.Pages[0].Dimensions.RightMargin * 2;
+
+
+            Chart chart = new Chart(0, 0, wdth, hght);
+            chart.BackgroundColor = RgbColor.AliceBlue;
+
+            PlotArea plotArea1 = chart.PlotAreas.Add(50, 10, 150, 200);
+            PlotArea plotArea2 = chart.PlotAreas.Add(250, 10, 150, 200);
+
+            IndexedBarSeries barSeries1 = new IndexedBarSeries("Item A");
+            plotArea1.Series.Add(barSeries1);
+            barSeries1.Values.Add(new float[] { 5, 7, 9, 6 });
+            IndexedBarSeries barSeries2 = new IndexedBarSeries("Item B");
+            plotArea1.Series.Add(barSeries2);
+            barSeries2.Values.Add(new float[] { 4, 2, 5, 8 });
+            IndexedBarSeries barSeries3 = new IndexedBarSeries("Item C");
+            plotArea1.Series.Add(barSeries3);
+            barSeries3.Values.Add(new float[] { 2, 4, 6, 9 });
+
+            IndexedBarSeries barSeries4 = new IndexedBarSeries("Item 1");
+            plotArea2.Series.Add(barSeries4);
+            barSeries4.Values.Add(new float[] { 3, 4, 6, 6 });
+            IndexedBarSeries barSeries5 = new IndexedBarSeries("Item 2");
+            plotArea2.Series.Add(barSeries5);
+            barSeries5.Values.Add(new float[] { 4, 2, 6, 7 });
+            IndexedBarSeries barSeries6 = new IndexedBarSeries("Item 3");
+            plotArea2.Series.Add(barSeries6);
+            barSeries6.Values.Add(new float[] { 2, 2, 6, 3 });
+
+            chart.AutoLayout = false;
+            chart.Legends[0].X = 50;
+            chart.Legends[0].BorderStyle = LineStyle.Solid;
+            chart.Legends[0].BorderColor = RgbColor.Black;
+            chart.Legends[0].Y = plotArea1.Height + 55;
+
+
+            page.Elements.Add(chart);
+            document.Draw(Util.GetPath("Output/barchart-two-plots-output.pdf"));
         }
 
     }

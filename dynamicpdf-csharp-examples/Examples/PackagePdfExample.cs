b3ac8939cd12fa8247762e7b8f1cf14e1261ec00
James A. Brannan <jamesabrannan@hushmail.com>
2024-11-19 16:08:52 -0500

    package example

--- dynamicpdf-csharp-examples/Examples/PackagePdfExample.cs
+++ dynamicpdf-csharp-examples/Examples/PackagePdfExample.cs
@@ -8,16 +8,20 @@ namespace DynamicPDFCoreSuite.Examples
         public static void Run()
         {
             Document document = new Document();
-
-            EmbeddedFile embeddedFile1 = new EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentA.pdf"));
-            EmbeddedFile embeddedFile2 = new EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentB.pdf"));
-            document.EmbeddedFiles.Add(embeddedFile1);
-            document.EmbeddedFiles.Add(embeddedFile2);
-
             Page page = new Page();
             document.Pages.Add(page);
 
-            document.Package = new DocumentPackage(AttachmentLayout.Detailed);
+            EmbeddedFile embeddedFile1 = new EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentA.pdf"));
+            EmbeddedFile embeddedFile2 = new EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentB.pdf"));
+            EmbeddedFile embeddedFile3 = new EmbeddedFile(Util.GetPath("Resources/Images/DPDFLogo.png"));
+            EmbeddedFile embeddedFile4 = new EmbeddedFile(Util.GetPath("Resources/Data/Doc1.docx"));
+            document.EmbeddedFiles.Add(embeddedFile1);
+            document.EmbeddedFiles.Add(embeddedFile2);
+            document.EmbeddedFiles.Add(embeddedFile3);
+            document.EmbeddedFiles.Add(embeddedFile4);
+
+
+            document.Package = new DocumentPackage(AttachmentLayout.Tile);
             document.Package.OrderBy = AttachmentListingOrderBy.Name;
             document.Package.AscendingOrder = false;
 
---
+++ dynamicpdf-vbnet-examples/Examples/PackagePdfExample.vb
@@ -0,0 +1,30 @@
+﻿Imports ceTe.DynamicPDF
+Imports ceTe.DynamicPDF.PageElements
+
+Namespace DynamicPDFCoreSuite.Examples
+    Public Class PackagePdfExample
+        Public Shared Sub Run()
+            Dim document As New Document()
+            Dim page As New Page()
+            document.Pages.Add(page)
+
+            Dim embeddedFile1 As New EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentA.pdf"))
+            Dim embeddedFile2 As New EmbeddedFile(Util.GetPath("Resources/PDFs/DocumentB.pdf"))
+            Dim embeddedFile3 As New EmbeddedFile(Util.GetPath("Resources/Images/DPDFLogo.png"))
+            Dim embeddedFile4 As New EmbeddedFile(Util.GetPath("Resources/Data/Doc1.docx"))
+            document.EmbeddedFiles.Add(embeddedFile1)
+            document.EmbeddedFiles.Add(embeddedFile2)
+            document.EmbeddedFiles.Add(embeddedFile3)
+            document.EmbeddedFiles.Add(embeddedFile4)
+
+            document.Package = New DocumentPackage(AttachmentLayout.Tile)
+            document.Package.OrderBy = AttachmentListingOrderBy.Name
+            document.Package.AscendingOrder = False
+
+            page.Elements.Add(New Label("Cover Page", 0, 0, 512, 40, Font.Helvetica, 30, TextAlign.Center, RgbColor.BlueViolet))
+
+            document.Draw(Util.GetPath("Output/package-pdf-output.pdf"))
+        End Sub
+    End Class
+End Namespace
+

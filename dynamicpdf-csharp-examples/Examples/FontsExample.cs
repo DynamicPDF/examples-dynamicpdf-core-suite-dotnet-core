42e6faf22cfc95fa15c1d08746e1aa2775c66420
James A. Brannan <jamesabrannan@hushmail.com>
2024-12-20 11:20:53 -0500

    added fonts code

--- dynamicpdf-csharp-examples/Examples/FontsExample.cs
+++ dynamicpdf-csharp-examples/Examples/FontsExample.cs
@@ -9,12 +9,32 @@ namespace DynamicPDFCoreSuite.Examples
     {
         public static void Run()
         {
-            ExampleOne();
-            ExampleTwo();
+            OpenTypeFontExample();
+            CJKExample();
+            GoogleFontsExample();
+            EncodingExample();
 
         }
 
-        public static void ExampleOne()
+        public static void EncodingExample()
+        {
+            Document document = new();
+            document.Pages.Add(new Page(PageSize.Letter));
+                      
+            Font centralEuropeHelveticaFont = new Helvetica(Encoder.CentralEurope);
+
+            Label lbl = new("čárka nad písmenem, silný přízvuk", 0, 0, 200, 12, centralEuropeHelveticaFont, 12);
+            Label lbl2 = new("čárka nad písmenem, silný přízvuk", 0, 100, 300, 0, Font.Helvetica, 12);
+           
+
+            document.Pages[0].Elements.Add(lbl);
+            document.Pages[0].Elements.Add(lbl2);
+
+            document.Draw(Util.GetPath("Output/encoding-font-example-one.pdf"));
+        }
+
+
+        public static void OpenTypeFontExample()
         {
             Document document = new Document();
 
@@ -26,11 +46,25 @@ namespace DynamicPDFCoreSuite.Examples
             // Use the OpenType font in a text area Page Element. 
             page.Elements.Add(new TextArea("Text", 0, 0, 200, 12, openTypeFont, 12));
 
-            document.Draw(Util.GetPath("Output/fonts-example-one.pdf"));
+            document.Draw(Util.GetPath("Output/opentype-font-example-one.pdf"));
+        }
+
+        public static void GoogleFontsExample()
+        {
+            Document document = new();
+            document.Pages.Add(new Page(PageSize.Letter));
+
+            GoogleFont googleFont = Font.Google("Roboto", false, false);
+
+
+            Label lbl = new Label("A Google Font Example.", 10, 10, 150, 50, googleFont, 22);
+            document.Pages[0].Elements.Add(lbl);
+
+            document.Draw(Util.GetPath("Output/googlefont-font-example-one.pdf"));
         }
 
 
-        public static void ExampleTwo()
+        public static void CJKExample()
         {
             Document document = new Document();
 
@@ -39,7 +73,7 @@ namespace DynamicPDFCoreSuite.Examples
 
             page.Elements.Add(new TextArea("CJK Text", 0, 0, 200, 12, Font.HeiseiKakuGothicW5, 16));
 
-            document.Draw(Util.GetPath("Output/fonts_example_two.pdf"));
+            document.Draw(Util.GetPath("Output/cjk-font-example-one.pdf"));
         }
 
     }

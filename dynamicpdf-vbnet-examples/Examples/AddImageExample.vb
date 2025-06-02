9a12d3833e220525dcea1e1b955ad71c6c11a8f5
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-02 10:21:04 -0400

    add image example

---
+++ dynamicpdf-vbnet-examples/Examples/AddImageExample.vb
@@ -0,0 +1,36 @@
+﻿Imports ceTe.DynamicPDF
+Imports ceTe.DynamicPDF.PageElements
+
+Public Class AddImageExample
+    Public Shared Sub Run()
+        ImageExample()
+        BackgroundImageExample()
+    End Sub
+
+    Public Shared Sub ImageExample()
+        Dim document As New Document()
+
+        Dim page As New Page()
+        document.Pages.Add(page)
+
+        Dim image As New Image(Util.GetPath("Resources/Images/DynamicPDF_top.gif"), 0, 0)
+        page.Elements.Add(image)
+
+        document.Draw(Util.GetPath("Output/images-one-example.pdf"))
+    End Sub
+
+    Public Shared Sub BackgroundImageExample()
+        Dim document As New Document()
+
+        Dim page As New Page()
+        document.Pages.Add(page)
+
+        Dim backgroundImage As New BackgroundImage(Util.GetPath("Resources/Images/DPDFLogo_Watermark.png"))
+        backgroundImage.UseMargins = True
+
+        page.Elements.Add(backgroundImage)
+
+        document.Draw(Util.GetPath("Output/background-image-example.pdf"))
+    End Sub
+End Class
+

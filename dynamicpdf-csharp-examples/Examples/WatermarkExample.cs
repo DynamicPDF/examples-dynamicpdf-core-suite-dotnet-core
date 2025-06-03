3af3a6b753f7e4f92513130287c3a3dd60c76afb
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-03 15:07:16 -0400

    watermark changes

--- dynamicpdf-csharp-examples/Examples/WatermarkExample.cs
+++ dynamicpdf-csharp-examples/Examples/WatermarkExample.cs
@@ -18,56 +18,34 @@ namespace DynamicPDFCoreSuite.Examples
         public static void ImageWaterMarkExample()
         {
             Document document = new Document();
-
             Page page = new Page();
             document.Pages.Add(page);
-
-            document.PdfVersion = PdfVersion.v1_6;
-
-            ImageWatermark imageWm = new ImageWatermark(GetImage());
-
+            ImageWatermark imageWm = new ImageWatermark(Util.GetPath("Resources/Images/large-logo.png"));
+            imageWm.ScaleX = .75f;
+            imageWm.ScaleY = .75f;
             page.Elements.Add(imageWm);
-
             document.Draw(Util.GetPath("Output/imagewatermark-output.pdf"));
         }
 
         public static void TextWatermarkExample()
         {
             Document document = new Document();
-
             Page page = new Page();
             document.Pages.Add(page);
-
-            document.PdfVersion = PdfVersion.v1_6;
-
             string text = "This is a text watermark.";
             TextWatermark twm = new TextWatermark(text);
-
             page.Elements.Add(twm);
-
             document.Draw(Util.GetPath("Output/textwatermark-output.pdf"));
         }
 
         public static void TemplateWatermarkExample()
         {
             MergeDocument document = new MergeDocument(Util.GetPath("Resources/PDFs/DocumentA.pdf"));
-
-            Image image = new Image(Util.GetPath("Resources/Images/DPDFLogo_Watermark.png"), 0, 0);
+            Image image = new Image(Util.GetPath("Resources/Images/stamp.png"), 0, 0);
             Template watermarkTemplate = new Template();
             watermarkTemplate.Elements.Add(image);
             document.Template = watermarkTemplate;
-
             document.Draw(Util.GetPath("Output/templatewatermark-output.pdf"));
-        }
-
-
-        private static byte[] GetImage()
-        {
-            FileStream inFile = new FileStream(Util.GetPath("Resources/Images/DPDFLogo_Watermark.png"), FileMode.Open, FileAccess.Read);
-            byte[] binaryData = new byte[inFile.Length];
-            inFile.Read(binaryData, 0, (int)inFile.Length);
-            inFile.Close();
-            return binaryData;
         }
     }
 }

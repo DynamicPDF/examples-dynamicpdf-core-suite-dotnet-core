e1c18c25f888b72eefba9db2ebdfa4fffed4c5be
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-04 15:12:26 -0400

    text encoding

--- dynamicpdf-csharp-examples/Examples/TextEncodingExample.cs
+++ dynamicpdf-csharp-examples/Examples/TextEncodingExample.cs
@@ -1,8 +1,6 @@
 ﻿using ceTe.DynamicPDF;
 using ceTe.DynamicPDF.PageElements;
 using ceTe.DynamicPDF.Text;
-using System.Text;
-using Encoder = ceTe.DynamicPDF.Text.Encoder;
 
 namespace DynamicPDFCoreSuite.Examples
 {
@@ -12,13 +10,12 @@ namespace DynamicPDFCoreSuite.Examples
         public static void Run()
         {
             Document document = new Document();
-
             Page page = new Page();
             document.Pages.Add(page);
-
-            Font centralEuropeHelveticaFont = new Helvetica(Encoder.CentralEurope);
-            page.Elements.Add(new TextArea("Text", 0, 0, 200, 12, centralEuropeHelveticaFont, 12));
-
+            string czech = "Tohle je textová zpráva, která říká: Ahoj, jak se máš?";
+            Font fnt1 = new Helvetica(Encoder.CentralEurope);
+            page.Elements.Add(new TextArea(czech, 0, 0, 300, 12, fnt1, 12));
+            page.Elements.Add(new TextArea(czech, 0, 20, 300, 12, Font.Helvetica, 12));
             document.Draw(Util.GetPath("Output/text-encoding-exampleOne.pdf"));
         }
 

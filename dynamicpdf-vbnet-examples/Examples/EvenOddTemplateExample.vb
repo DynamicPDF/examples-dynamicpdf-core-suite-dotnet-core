e5972078cf60b5b596cdf5aefdc448bb6b6e9ab4
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-04 17:01:23 -0400

    modified templates

--- dynamicpdf-vbnet-examples/Examples/EvenOddTemplateExample.vb
+++ dynamicpdf-vbnet-examples/Examples/EvenOddTemplateExample.vb
@@ -2,8 +2,29 @@
 Imports ceTe.DynamicPDF.PageElements
 
 Namespace DynamicPDFCoreSuite.Examples
+
     Class EvenOddTemplateExample
+
         Public Shared Sub Run()
+            Simple()
+            Documentation()
+        End Sub
+
+        Public Shared Sub Simple()
+            Dim myDoc As New Document()
+            myDoc.Pages.Add(New Page())
+            myDoc.Pages.Add(New Page())
+            myDoc.Pages.Add(New Page())
+
+            Dim tmp As New EvenOddTemplate()
+            tmp.EvenElements.Add(New Label("ODD", 0, 0, 200, 12))
+            tmp.OddElements.Add(New Label("EVEN", 0, 0, 200, 12))
+
+            myDoc.Template = tmp
+            myDoc.Draw(Util.GetPath("Output/simple-evenodd-out.pdf"))
+        End Sub
+
+        Public Shared Sub Documentation()
             Dim myDoc As New Document()
             myDoc.Pages.Add(New Page())
             myDoc.Pages.Add(New Page())
@@ -17,5 +38,8 @@ Namespace DynamicPDFCoreSuite.Examples
             myDoc.Template = tmp
             myDoc.Draw(Util.GetPath("Output/evenOddtemplateExample-out.pdf"))
         End Sub
+
     End Class
+
 End Namespace
+
--- dynamicpdf-vbnet-examples/Examples/TemplateExample.vb
+++ dynamicpdf-vbnet-examples/Examples/TemplateExample.vb
@@ -1,9 +1,27 @@
 ﻿Imports ceTe.DynamicPDF
 Imports ceTe.DynamicPDF.PageElements
 
-Namespace DynamicPDFCoreSuite.Examples
-    Class TemplateExample
+Class TemplateExample
+
         Public Shared Sub Run()
+            Simple()
+            One()
+        End Sub
+
+        Public Shared Sub Simple()
+            Dim document As New Document()
+            Dim page As New Page()
+            document.Pages.Add(page)
+
+            Dim template As New Template()
+            template.Elements.Add(New Label("Header", 0, 0, 200, 12))
+            template.Elements.Add(New Image(Util.GetPath("Resources/Images/DPDFLogo.png"), 100, 0))
+
+            document.Template = template
+            document.Draw(Util.GetPath("Output/template-out.pdf"))
+        End Sub
+
+        Public Shared Sub One()
             Dim doc As New Document()
             Dim tmp As New Template()
 
@@ -19,9 +37,12 @@ Namespace DynamicPDFCoreSuite.Examples
             tmp.Elements.Add(New Rectangle(x, y, 20, hgt, RgbColor.Navy, RgbColor.Navy))
             tmp.Elements.Add(New Image(Util.GetPath("Resources/Images/") & "DPDFLogo.png", x + 30, y + 5))
             tmp.Elements.Add(New Label("Test.", 200, 300, 200, 0))
+
             doc.Template = tmp
 
             doc.Draw(Util.GetPath("Output/addtemplatetopdf-out.pdf"))
         End Sub
+
     End Class
-End Namespace
+
+

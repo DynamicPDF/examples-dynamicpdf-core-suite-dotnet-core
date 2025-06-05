01f687c2ee524a2a3b95473d3a21ff4cc7873af4
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-05 10:40:04 -0400

    tagged pdf example

--- dynamicpdf-csharp-examples/Examples/TaggedPdfExample.cs
+++ dynamicpdf-csharp-examples/Examples/TaggedPdfExample.cs
@@ -12,27 +12,16 @@ namespace DynamicPDFCoreSuite.Examples
         {
             Document document = new Document();
             document.Tag = new TagOptions(true);
-
             Page page = new Page();
             document.Pages.Add(page);
-
             Image image = new Image(Util.GetPath("Resources/Images/DynamicPDF_top.gif"), 180f, 150f, 0.24f);
             image.Height = 200;
             image.Width = 200;
-
             StructureElement imageStructureElement = new StructureElement(TagType.Figure);
             imageStructureElement.IncludeDefaultAttributes = true;
             imageStructureElement.AlternateText = "DynamicPDF Logo";
-
             image.Tag = imageStructureElement;
             page.Elements.Add(image);
-            PageNumberingLabel pageNumberingLabel = new PageNumberingLabel("Page %%CP%% of %%TP%%", 0, 680, 512, 12, Font.Helvetica, 12, TextAlign.Center);
-
-            Artifact artifact = new Artifact();
-            artifact.SetType(ArtifactType.Pagination);
-            pageNumberingLabel.Tag = artifact;
-            page.Elements.Add(pageNumberingLabel);
-
             document.Draw(Util.GetPath("Output/tagged-pdf-output.pdf"));
         }
 

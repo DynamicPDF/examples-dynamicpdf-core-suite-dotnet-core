4350b87fcdb81476d8ba1212b3d6b4cf2b327dde
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-11 12:32:21 -0400

    password protect example

---
+++ dynamicpdf-vbnet-examples/Examples/PasswordProtectPDF.vb
@@ -0,0 +1,38 @@
+﻿Imports ceTe.DynamicPDF
+Imports ceTe.DynamicPDF.Cryptography
+Imports ceTe.DynamicPDF.Merger
+Imports ceTe.DynamicPDF.PageElements
+
+Namespace DynamicPDFCoreSuite.Examples
+
+    Class PasswordProtectPDF
+
+        Public Shared Sub Run()
+            Generator()
+            Merger()
+        End Sub
+
+        Public Shared Sub Generator()
+            Dim document As New Document()
+            Dim page As New Page()
+            page.Elements.Add(New Label("Protected", 0, 0, 100, 15))
+            document.Pages.Add(page)
+
+            Dim security As New Aes256Security("OwnerPassword", "UserPassword")
+            document.Security = security
+
+            document.Draw(Util.GetPath("Output/PasswordProtectNewPDF.pdf"))
+        End Sub
+
+        Public Shared Sub Merger()
+            Dim document As New MergeDocument(Util.GetPath("Resources/PDFs/DocumentA.pdf"))
+            Dim security As New Aes256Security("OwnerPassword", "UserPassword")
+            document.Security = security
+
+            document.Draw(Util.GetPath("Output/PasswordProtectExistingPDF.pdf"))
+        End Sub
+
+    End Class
+
+End Namespace
+

796bbc5ba3033c2ee7caac56923669a6798a4822
James A. Brannan <jamesabrannan@hushmail.com>
2025-06-11 15:23:39 -0400

    layoutgrid to vb

---
+++ dynamicpdf-vbnet-examples/Examples/LayoutGridExample.vb
@@ -0,0 +1,20 @@
+﻿Imports ceTe.DynamicPDF
+Imports ceTe.DynamicPDF.PageElements
+
+Namespace DynamicPDFCoreSuite.Examples
+    Class LayoutGridExample
+
+        Public Shared Sub Run()
+            Dim document As New Document()
+            Dim page As New Page()
+            document.Pages.Add(page)
+
+            Dim grid As New LayoutGrid(LayoutGrid.GridType.Decimal)
+            page.Elements.Add(New Rectangle(50, 50, 50, 50, 2))
+            page.Elements.Add(grid)
+
+            document.Draw(Util.GetPath("Output/layout-grid-output.pdf"))
+        End Sub
+
+    End Class
+End Namespace
